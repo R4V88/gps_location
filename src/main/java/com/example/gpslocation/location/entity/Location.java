05eb6f3dae5d24388eff81a53ac03fa95358f7c3
Rafał Sochacki <sochacki.rfl@gmail.com>
2022-06-07 19:46:59 +0200

    Location entity

---
+++ src/main/java/com/example/gpslocation/location/entity/Location.java
@@ -0,0 +1,39 @@
+package com.example.gpslocation.location.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EntityListeners({AuditingEntityListener.class})
+public class Location {
+
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @CreatedDate
+    LocalDateTime createdAt;
+
+    Long deviceId;
+
+    Long latitude;
+
+    Long longitude;
+}
