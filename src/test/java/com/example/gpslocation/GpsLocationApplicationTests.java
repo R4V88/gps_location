9502bafe87ef230c5b3f66e9660b42f73e9400a1
Rafał Sochacki <sochacki.rfl@gmail.com>
2022-06-07 19:46:24 +0200

    Initial commit

---
+++ src/test/java/com/example/gpslocation/GpsLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gpslocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsLocationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
